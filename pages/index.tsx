5bee0858edce182d146733b1677cc813473b5d6f
codeFrite <codefrite@gmail.com>
2024-10-22 14:01:43 +0200

    trying to solve the image not found issue when deploying on github pages by changing images path

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,13 +23,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
-      >
+      <div className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}>
         <main className={styles.main}>
           <Image
             className={styles.logo}
-            src="/next.svg"
+            src="./next.svg"
             alt="Next.js logo"
             width={180}
             height={38}
@@ -47,11 +45,10 @@ export default function Home() {
               className={styles.primary}
               href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
-              rel="noopener noreferrer"
-            >
+              rel="noopener noreferrer">
               <Image
                 className={styles.logo}
-                src="/vercel.svg"
+                src="vercel.svg"
                 alt="Vercel logomark"
                 width={20}
                 height={20}
@@ -62,8 +59,7 @@ export default function Home() {
               href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
-              className={styles.secondary}
-            >
+              className={styles.secondary}>
               Read our docs
             </a>
           </div>
@@ -72,43 +68,22 @@ export default function Home() {
           <a
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
+            rel="noopener noreferrer">
+            <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
             Learn
           </a>
           <a
             href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
+            rel="noopener noreferrer">
+            <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
             Examples
           </a>
           <a
             href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
+            rel="noopener noreferrer">
+            <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
             Go to nextjs.org →
           </a>
         </footer>
