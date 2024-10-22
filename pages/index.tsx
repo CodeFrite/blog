3ea623ac31bf53f5c8e07c8d546247bb2442b296
codeFrite <codefrite@gmail.com>
2024-10-22 14:05:53 +0200

    in order to work on github pages, images & fonts links should be relative using either ./ or nothing before the uri of ressource located in /public folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,7 +48,7 @@ export default function Home() {
               rel="noopener noreferrer">
               <Image
                 className={styles.logo}
-                src="vercel.svg"
+                src="./vercel.svg"
                 alt="Vercel logomark"
                 width={20}
                 height={20}
@@ -69,21 +69,21 @@ export default function Home() {
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer">
-            <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
+            <Image aria-hidden src="./file.svg" alt="File icon" width={16} height={16} />
             Learn
           </a>
           <a
             href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer">
-            <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
+            <Image aria-hidden src="./window.svg" alt="Window icon" width={16} height={16} />
             Examples
           </a>
           <a
             href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer">
-            <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
+            <Image aria-hidden src="./globe.svg" alt="Globe icon" width={16} height={16} />
             Go to nextjs.org →
           </a>
         </footer>
